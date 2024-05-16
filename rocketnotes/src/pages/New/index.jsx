904a30302dbf8e3a206cc6065ca2af79b29fabe4
Paulo-xavier-a <pr3546287@gmail.com>
2024-05-16 11:41:04 -0300

    criando textarea

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -2,6 +2,7 @@ import {Header} from '../../components/Header'
 import {Container} from './styles'
 import { Form } from './styles'
 import {Input} from '../../components/Input'
+import {Textarea} from '../../components/Textarea'
 
 export function New(){
     return(
@@ -13,9 +14,8 @@ export function New(){
                         <h1>Criar nota</h1>
                         <a href="/">Voltar</a>
                     </header>
-                    <Input
-                    placeholder="titulo"
-                    />
+                    <Input placeholder="titulo"/>
+                <Textarea placeholder="Obsevações"/>
                 </Form>
             </main>
         </Container>
