c14be222455e0704f789a93192181af7725e34a2
Paulo-xavier-a <pr3546287@gmail.com>
2024-05-16 10:34:57 -0300

    Criando titulo no NEW

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,10 +1,23 @@
 import {Header} from '../../components/Header'
 import {Container} from './styles'
+import { Form } from './styles'
+import {Input} from '../../components/Input'
 
 export function New(){
     return(
         <Container>
             <Header/>
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">Voltar</a>
+                    </header>
+                    <Input
+                    placeholder="titulo"
+                    />
+                </Form>
+            </main>
         </Container>
     )
 }
--- rocketnotes/src/pages/New/styles.js
+++ rocketnotes/src/pages/New/styles.js
@@ -10,3 +10,18 @@ grid-template-areas:
 "header"
 "content";
 `
+export const Form = styled.form`
+max-width: 550px;
+margin: 38px auto;
+
+>header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 36px ;
+
+    >a{
+        color: ${({theme}) => theme.COLORS.GRAY_100};
+    }
+}
+`
