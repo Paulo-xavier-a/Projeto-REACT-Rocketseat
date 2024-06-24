2b392c22550eb8b22bef749c949de18a39fd2b78
Paulo-xavier-a <pr3546287@gmail.com>
2024-06-24 17:26:51 -0300

    Finalizando new

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -1,23 +1,42 @@
-import {Header} from '../../components/Header'
-import {Container} from './styles'
-import { Form } from './styles'
-import {Input} from '../../components/Input'
-import {Textarea} from '../../components/Textarea'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import {Button } from '../../components/Button'
 
-export function New(){
-    return(
-        <Container>
-            <Header/>
-            <main>
-                <Form>
-                    <header>
-                        <h1>Criar nota</h1>
-                        <a href="/">Voltar</a>
-                    </header>
-                    <Input placeholder="titulo"/>
-                <Textarea placeholder="Obsevações"/>
-                </Form>
-            </main>
-        </Container>
-    )
+
+import { Container, Form } from './styles'
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
+
+          <Section title="links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+          <Section title="Marcadores">
+            <div className="tags">
+
+            <NoteItem value="react"/>
+            <NoteItem isNew placeholder="Nova Tag"/>
+            </div>
+          </Section>
+          <Button title="salvar"/>
+        </Form>
+      </main>
+    </Container>
+  )
 }
